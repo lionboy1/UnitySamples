2cc38bd2ccaef4f59df44181490e7b942bf2da62
lionboy1 <great.studios30@gmail.com>
2019-11-17 20:22:39 -0500

    initial commit

---
+++ Assets/scripts/Goal.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+//Vuforia project
+
+namespace AR
+{
+	public class Goal : MonoBehaviour
+    {
+        LineRenderer line;
+
+        void Start()
+        {
+            line = GetComponent<LineRenderer>();
+            line.SetWidth( .05f, .05f);
+        }
+
+        void Update()
+        {
+            //Current position of marker
+            line.SetPosition(0, transform.parent.position);
+            //Marker position relative to surface.
+            line.SetPosition(1, new Vector3(transform.parent.position.x, 0, transform.parent.position.z));
+	}
+     }		
+}
